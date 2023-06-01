a4cf258bf6317beb5b9d109dd5bfccebd0c9c21f
voyagerdva <mariner-9@yandex.ru>
2023-06-01 18:21:15 +0300

    dmdev 10 IO 4

--- src/e0126_dmdev_level2_10_In_Output/WriterRunner25.java
+++ src/e0126_dmdev_level2_10_In_Output/WriterRunner25.java
@@ -4,16 +4,21 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class WriterRunner25 {
     public static void main(String[] args) throws IOException {
-        File file = Path.of("resources", "writer.poem").toFile();
-        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
-            fileWriter.append("Hello World@");
-            fileWriter.newLine();
-            fileWriter.append("Java");
+//        File file = Path.of("resources", "writer.poem").toFile();
+        Path path = Path.of("resources", "writer2.poem");
+//        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
 
-        }
+        Files.write(path, List.of("Hello world!", "Java"));
+//        try (BufferedWriter fileWriter = Files.newBufferedWriter(path)) {
+//            fileWriter.append("Hello World@");
+//            fileWriter.newLine();
+//            fileWriter.append("Java");
+//        }
     }
 }
