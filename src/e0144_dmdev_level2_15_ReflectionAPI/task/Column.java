ef06b0ef3c552a0501b4a7acbf2a13e7dd8ab4bc
voyagerdva <mariner-9@yandex.ru>
2023-06-25 16:18:08 +0300

    reflectionAPI Practice

---
+++ src/e0144_dmdev_level2_15_ReflectionAPI/task/Column.java
@@ -0,0 +1,13 @@
+package e0144_dmdev_level2_15_ReflectionAPI.task;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+
+    String name();
+}
