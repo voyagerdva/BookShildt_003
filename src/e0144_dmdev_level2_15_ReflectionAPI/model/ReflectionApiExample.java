9ae3f34d74586e311fde3e3101bae62525d79822
voyagerdva <mariner-9@yandex.ru>
2023-06-25 13:37:35 +0300

    reflectionAPI

---
+++ src/e0144_dmdev_level2_15_ReflectionAPI/model/ReflectionApiExample.java
@@ -0,0 +1,26 @@
+package e0144_dmdev_level2_15_ReflectionAPI.model;
+
+public class ReflectionApiExample {
+    public static void main(String[] args) {
+        User user = new User(25L, "Ivan");
+        Class<? extends User> userClass = user.getClass();
+        Class<User> userClass1 = User.class;
+        System.out.println(userClass == userClass1);
+
+    }
+
+    private class Test1 {
+
+
+    }
+
+    private static class Test3 {
+
+    }
+
+    private enum Test2 {
+
+    }
+
+
+}
